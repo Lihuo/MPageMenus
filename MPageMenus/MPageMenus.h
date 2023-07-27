45dd2a601dac5dae76612baa1d10aeff98e42c14
Lihour <lihou.kong@lyhourpay.com>
2023-07-27 23:03:01 +0700

    Initail project setup

---
+++ MPageMenus/MPageMenus.h
@@ -0,0 +1,18 @@
+//
+//  MPageMenus.h
+//  MPageMenus
+//
+//  Created by Lihour on 27/7/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MPageMenus.
+FOUNDATION_EXPORT double MPageMenusVersionNumber;
+
+//! Project version string for MPageMenus.
+FOUNDATION_EXPORT const unsigned char MPageMenusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MPageMenus/PublicHeader.h>
+
+
